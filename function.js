4c47934e7cbd8e6eb424b56749102e6e0657d4a8
zhangchongjie <778280151@qq.com>
2021-10-09 10:07:29 +0800

    first commit

---
+++ function.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    $("#header").remove()
+    let nr = "娃哈哈AD钙";
+    console.log('fffff')
+    printpage(nr)
+}, 3000)
+
+/**
+ * 弹出打印框
+ * @param {需要打印的html拼接字符串} html 
+ */
+function printpage(html) {
+    var newWindow = window.open('', '_blank', 'width=1,height=1,top=10000,left=10000');
+    // var html = "";// 这里的html可由别处传参，也可自己去接口获取
+    newWindow.document.write(html);
+    newWindow.document.close();
+    newWindow.print();
+    newWindow.close();
+}
+// dom[0].remove()
